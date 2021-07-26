4b1aedbe37de7ee84a828000b9f33df717f35f4f
kevin <kevin_captain@163.com>
2021-07-26 19:58:25 +0800

    mod: 修改域名

--- src/config/config.dev.js
+++ src/config/config.dev.js
@@ -1,7 +1,7 @@
 let BASE_PATH = ''; // 本地测试
 
 if (process.env.NODE_ENV === 'production') {
-  BASE_PATH = 'http://test-api.firshop.com'; // 线上测试环境
+  BASE_PATH = 'http://sensorapi.norra.cn:9110'; // 线上测试环境
 }
 
 export default {
